783922bccc82ec29b78444c1f86d75dfdcb80298
Pedro Varela <pedrao.varela@gmail.com>
2024-12-08 22:41:32 -0300

    refactor: menu links

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 
 export const Menu = () => {
   return (
-    <div className="w-full flex justify-center items-center border border-gray-200s">
+    <div className="w-full bg-white z-20 flex sticky top-0 justify-center items-center border border-gray-200s">
       <div className="flex p-6 max-w-[1024px] w-full justify-between">
-        <div className="flex gap-4">
+        <Link href={"/"} className="flex gap-4 items-center">
           <Image
             src="/logo_img.png"
             alt="Logo henrique castriciano"
-            width={40}
-            height={40}
+            width={60}
+            height={60}
           />
           <div className="text-green-950 font-bold hidden md:block">
             <p>Henrique</p>
             <p>Castriciano</p>
           </div>
-        </div>
+        </Link>
         <div className="flex gap-4">
           <Link
             href={"/"}
@@ -31,7 +31,7 @@ export const Menu = () => {
             <p>Home</p>
           </Link>
           <Link
-            href={"/"}
+            href={"/historia"}
             className="flex gap-2 justify-center items-center hover:text-green-900"
           >
             <Image
